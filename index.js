9280e0be50953924c23a1dce479c25cfd1e1d657
Matteo Bonacini <matteo.bonna@gmail.com>
2018-06-27 16:11:59 +0200

    mods

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const Valkyrie = require('aws-valkyrie');
+const Valkyrie = require("aws-valkyrie");
 const app = new Valkyrie();
-const pkg = require('./package.json');
+const AWS = require("aws-sdk");
+
+const dynamodb = new AWS.DynamoDB();
+
+const env = "staging";
 
 //body parser middleware; it will become an external module;
 app.use((req, res, next) => {
   try {
-    if (req.get('content-type').toLowerCase().includes('application/json')) {
+    if (req.get("content-type").toLowerCase().includes("application/json")) {
       req.body = JSON.parse(req.body);
     }
   }
@@ -13,12 +17,25 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/payload-test', (req, res) => res.json(Object.assign({digestedBy: 'Valkyrie'}, req.body)));
+app.get("/", (req, res) => res.send("this is cryptomon"));
 
-app.get('/info', (req, res) => res.json(pkg));
+app.get("/monstersOfAddress", ({body: {address}}, res) => {
+  if (!address) return res.sendStatus(400);
 
-app.get('/home', (req, res) => res.send('this is cryptomon'));
+  const params = {
+    ConsistentRead: false,
+    Key: {
+      address //TODO check for valid address before?
+    },
+    TableName: `cryptomon-monsters-${env}`
+  };
 
-app.all('*', (req, res) => res.sendStatus(404));
+  dynamodb.getItem(params)
+    .promise()
+    .then(console.log)
+    .catch(console.error);
+});
+
+app.all("*", (req, res) => res.sendStatus(404));
 
 exports.handler = (...args) => app.listen(...args);
