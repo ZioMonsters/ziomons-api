176873d985fcdbf98d3562672ea0f9604fa0d5d8
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-06-27 14:38:30 +0200

    fist commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#cryptomon-api
+
+
---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Valkyrie = require('aws-valkyrie');
+const app = new Valkyrie();
+const pkg = require('./package.json');
+
+//body parser middleware; it will become an external module;
+app.use((req, res, next) => {
+  try {
+    if (req.get('content-type').toLowerCase().includes('application/json')) {
+      req.body = JSON.parse(req.body);
+    }
+  }
+  catch (ignore) {}
+  next();
+});
+
+app.post('/payload-test', (req, res) => res.json(Object.assign({digestedBy: 'Valkyrie'}, req.body)));
+
+app.get('/info', (req, res) => res.json(pkg));
+
+app.get('/home', (req, res) => res.send('hello world'));
+
+app.all('*', (req, res) => res.sendStatus(404));
+
+exports.handler = (...args) => app.listen(...args);
