89837e38eb8e0ecc3600ae17f7c224d4176f4148
Nicolò Fuccella <nico.fuccella@gmail.com>
2018-09-08 09:25:33 +0200

    getMonster

--- index.js
+++ index.js
@@ -1,66 +1,134 @@
-const Valkyrie = require("aws-valkyrie");
-const app = new Valkyrie();
-const AWS = require("aws-sdk");
-const dynamodb = new AWS.DynamoDB({ region: "eu-west-3" });
-const pkg = require('./package.json');
+const Valkyrie = require("aws-valkyrie")
+const app = new Valkyrie()
+const AWS = require("aws-sdk")
+const dynamodb = new AWS.DynamoDB({ region: "eu-west-3" })
+const documentClient = new AWS.DynamoDB.DocumentClient({ region: "eu-west-3" })
+const pkg = require("./package.json")
 
-const env = "production";
-const paginatorLimit = 32;
+const env = "production"
+const paginatorLimit = 32
 
-//body parser middleware; it will become an external module;
-app.use((req, res, next) => {
-  try {
-    if (req.get("content-type").toLowerCase().includes("application/json")) {
-      req.body = JSON.parse(req.body);
-    }
-  }
-  catch (ignore) {}
-  next();
-});
+;["body-parser", "query-parser", "res-error"].forEach(middleware => app.use(require(`./middlewares/${middleware}.js`)()))
 
-//Used to prevent errors when trying to destructure queryStringParameters from lambda
-app.use((req, res, next) => {
-  if (!req.queryStringParameters)
-    req.queryStringParameters = {};
-  next();
-});
+app.get("/home", (req, res) => res.send("This is cryptomon"))
 
-app.get("/home", (req, res) => res.send("This is cryptomon"));
+app.get("/info", (req, res) => res.json(pkg))
 
-app.get('/info', (req, res) => res.json(pkg));
+// returns an array of objects ({ id, attack, defence, speed, experience, level, rarity })
+// da mettere validation del limit e del next nel middleware di validation
+app.get("/listMonsters", ({ query: { address, next, limit = 50 } }, res) => {
 
-app.get("/monstersOfAddress", ({queryStringParameters: { address, ExclusiveStartKey }}, res) => {
-  if (!/^0x[a-fA-F0-9]{40}$/.test(address)) return res.status(400).send("Invalid address parameter.");
+  // da spostare in un middleware di validation
+  // if (!/^0x[a-fA-F0-9]{40}$/.test(address)) return res.status(400).send("Invalid address parameter.")
 
-  if (ExclusiveStartKey) {
-    try {
-      ExclusiveStartKey = JSON.parse(ExclusiveStartKey)
-    } catch (err) {
-      return res.status(400).send("Invalid ExclusiveStartKey parameter.")
-    }
-  }
+  // if (ExclusiveStartKey) {
+  //   try {
+  //     ExclusiveStartKey = JSON.parse(ExclusiveStartKey)
+  //   } catch (err) {
+  //     return res.status(400).send("Invalid ExclusiveStartKey parameter.")
+  //   }
+  // }
 
-  const params = {
-    TableName: `cryptomon-monsters-${env}`,
-    Limit: paginatorLimit,
-    ExclusiveStartKey,
-    ExpressionAttributeValues: {
-      ":a" : {
-        S: address.substr(2)
+  documentClient.query({
+    TableName: `cryptomon-monsters-${process.env.NODE_ENV}`,
+    IndexName: "user-monsterId-index",
+    Limit: limit,
+    ...(next ? { ExclusiveStartKey: Object.assign({}, next, {
+      address
+    }) } : {}),
+    KeyConditions: {
+      user: {
+        ComparisonOperator: "EQ",
+        AttributeValueList: [address]
       }
     },
-    KeyConditionExpression: "address = :a"
-  };
+    ExpressionAttributeNames: {
+      "#monsterId": "monsterId",
+      "#attack": "attack",
+      "#defence": "defence",
+      "#speed": "speed",
+      "#experience": "experience",
+      "#level": "level",
+      "#rarity": "rarity"
+    },
+    ProjectionExpression: "#monsterId, #attack, #defence, #speed, #experience, #level, #rarity"
+  }).promise()
+    .then(({ Items, Count: count }) => res.json({
+      count,
+      monsters: Items.map(item => {
+        item.id = item.monsterId
+        delete item.monsterId
+        return item
+      })
+    }))
+    .catch(err => {
+      console.error(err)
+      res.send(err)
+    })
+  //
+  // const params = {
+  //   TableName: `cryptomon-monsters-${env}`,
+  //   Limit: paginatorLimit,
+  //   ExclusiveStartKey,
+  //   ExpressionAttributeValues: {
+  //     ":a" : {
+  //       S: address.substr(2)
+  //     }
+  //   },
+  //   KeyConditionExpression: "address = :a"
+  // }
+  //
+  // dynamodb.query(params)
+  //   .promise()
+  //   .then(e => res.status(200).json(e))
+  //   .catch(e => res.status(500).json(e))
+})
 
-  dynamodb.query(params)
-    .promise()
-    .then(e => res.status(200).json(e))
-    .catch(e => res.status(500).json(e));
-});
+app.get("/getMonster", ({ query: { monsterId }}, res) => {
+  documentClient.get({
+    TableName: `cryptomon-monsters-${process.env.NODE_ENV}`,
+    Key: {
+      monsterId
+    }
+  }).promise()
+    .then(data => res.json(data.Item))
+    .catch(err => res.error(err))
+})
 
-app.get("/battlesOfAddress", ({queryStringParameters: {address, ExclusiveStartKey}}, res) => {
-  if (!/^0x[a-fA-F0-9]{40}$/.test(address)) return res.status(400).send("Invalid address.");
+// returns an array of objects ({ clashId, opponent, userTeam, opponentTeam, bonusWinner, result, bet })
+// WON, LOST, DRAW
+app.get("/listClashes", ({ query: { address, next, limit = 50 } }, res) => {
+  // if (!/^0x[a-fA-F0-9]{40}$/.test(address)) return res.status(400).send("Invalid address.")
 
+  const params = {
+    TableName: `cryptomon-clashes-${process.env.NODE_ENV}`,
+    IndexName: "user-clashId-index",
+    Limit: limit,
+    ...(next ? { ExclusiveStartKey: Object.assign({}, next, { user: address }) } : {}),
+    KeyConditions: {
+      user: {
+        ComparisonOperator: "EQ",
+        AttributeValueList: [address]
+      }
+    },
+    ExpressionAttributeNames: {
+      "#clashId": "clashId",
+      "#opponent": "opponent",
+      "#userTeam": "userTeam",
+      "#opponentTeam": "opponentTeam",
+      "#bonusWinner": "bonusWinner",
+      "#result": "result",
+      "#bet": "bet"
+    },
+    ProjectionExpression: "#clashId, #opponent, #userTeam, #opponentTeam, #bonusWinner, #result, #bet"
+  }
+
+  documentClient.query(params).promise()
+    .then(({ Items, Count }) => res.json({ count: Count, clashes: Items }))
+    .catch(err => res.error(err))
+})
+
+app.get("/battleInfo", ({ queryStringParameters: { eventHash, ExclusiveStartKey } }, res) => {
   if (ExclusiveStartKey) {
     try {
       ExclusiveStartKey = JSON.parse(ExclusiveStartKey)
@@ -71,34 +139,7 @@ app.get("/battlesOfAddress", ({queryStringParameters: {address, ExclusiveStartKe
 
   const params = {
     TableName: `cryptomon-battles-${env}`,
-    Limit: paginatorLimit,
-    ExclusiveStartKey,
-    ExpressionAttributeValues: {
-      ":a" : {
-        S: address.substr(2)
-      }
-    },
-    KeyConditionExpression: "address = :a"
-  };
-
-  dynamodb.query(params)
-    .promise()
-    .then(e => res.status(200).json(e))
-    .catch(e => res.status(500).json(e));
-});
-
-app.get("/battleInfo", ({queryStringParameters: {eventHash, ExclusiveStartKey}}, res) => {
-  if (ExclusiveStartKey) {
-    try {
-      ExclusiveStartKey = JSON.parse(ExclusiveStartKey)
-    } catch (err) {
-      return res.status(400).send("Invalid ExclusiveStartKey parameter.")
-    }
-  }
-
-  const params = {
-    TableName: `cryptomon-battles-${env}`,
-    IndexName: `hashIndex`,
+    IndexName: "hashIndex",
     Limit: paginatorLimit,
     ExclusiveStartKey,
     ExpressionAttributeValues: {
@@ -107,15 +148,15 @@ app.get("/battleInfo", ({queryStringParameters: {eventHash, ExclusiveStartKey}},
       }
     },
     KeyConditionExpression: "eventHash = :h"
-  };
+  }
 
   dynamodb.query(params)
     .promise()
     .then(e => res.status(200).json(e))
-    .catch(e => res.status(500).json(e));
-});
+    .catch(e => res.status(500).json(e))
+})
 
-app.get("/monstersInSale", ({queryStringParameters: {ExclusiveStartKey}}, res) => {
+app.get("/monstersInSale", ({ queryStringParameters: { ExclusiveStartKey } }, res) => {
   if (ExclusiveStartKey) {
     try {
       ExclusiveStartKey = JSON.parse(ExclusiveStartKey)
@@ -127,14 +168,14 @@ app.get("/monstersInSale", ({queryStringParameters: {ExclusiveStartKey}}, res) =
     TableName: `cryptomon-shop-${env}`,
     ExclusiveStartKey,
     Limit: paginatorLimit
-  };
+  }
 
   dynamodb.scan(params)
     .promise()
     .then(e => res.status(200).json(e))
-    .catch(e => res.status(500).json(e));
-});
+    .catch(e => res.status(500).json(e))
+})
 
-app.all("*", (req, res) => res.sendStatus(404));
+app.all("*", (req, res) => res.sendStatus(404))
 
-exports.handler = (...args) => app.listen(...args);
+exports.handler = (...args) => app.listen(...args)
